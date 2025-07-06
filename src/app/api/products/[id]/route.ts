d6cf4f579418e9ac00e3ec0fc5ac2993d6da6492
Adarsh Roy <adarshroy2010@gmail.com>
2025-07-06 15:19:17 +0530

    depl. backend err fix

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,20 +1,31 @@
-// api/products/[id]/routes.ts
 import ProductModel from '@/models/Product';
 import { connectDB } from "@/lib/db";
 import { NextResponse } from 'next/server';
 
-export async function GET(req:Request,{params}:{params:{id:string}}) {
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
   try {
     await connectDB();
-    const product = await ProductModel.findById(params.id) ; 
+    
+    // Await the params promise
+    const { id } = await params;
+    
+    const product = await ProductModel.findById(id);
+    
     if (!product) {
-      return NextResponse.json({error: 'Product not found from this id'},{status: 404})
+      return NextResponse.json(
+        { error: 'Product not found from this id' },
+        { status: 404 }
+      );
     }
 
-    return NextResponse.json(product) ;
-  } catch (err:any) {
-    return NextResponse.json({ error: 'Failed to fetch product', details: err.message }, { status: 500 });
+    return NextResponse.json(product);
+  } catch (err: any) {
+    return NextResponse.json(
+      { error: 'Failed to fetch product', details: err.message },
+      { status: 500 }
+    );
   }
 }
-
-
